d21ae5a8c1f393d4cbd607158b79b0db857e9691
Zaheer khan <zk453810@gmail.com>
2025-09-03 03:23:09 +0530

    updating crossorigin for production

--- src/main/java/com/notesapi/NoteApp/Security/SecurityConfig.java
+++ src/main/java/com/notesapi/NoteApp/Security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000")); // restrict to frontend
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000" , "https://notes-app-frontend-zeta-seven.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
